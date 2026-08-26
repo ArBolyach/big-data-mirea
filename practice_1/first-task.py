363b9a31359be51862de822824ed142677c838fb
Artyom Bolyachinov <artembolyachinov@gmail.com>
2026-08-26 19:07:19 +0300

    fixed first task && added solution for second task

--- practice_1/first-task.py
+++ practice_1/first-task.py
@@ -7,21 +7,21 @@ import math
 
 class FigureArea:
     @staticmethod
-    def getSquareArea(a:int,b:int)->int:
+    def getSquareArea(a:float,b:float)->float:
         return a*b
     
     @staticmethod
-    def getTriangleArea(a:int, h:int)->int:
+    def getTriangleArea(a:float, h:float)->float:
         return (a*h)/2
     
     @staticmethod
-    def getCircleArea(r:int)->int:
+    def getCircleArea(r:float)->float:
         return math.pi*(r**2)
     
 
-circleArea:int = FigureArea.getCircleArea(2)
-triangleArea:int = FigureArea.getTriangleArea(5,2)
-squareArea:int = FigureArea.getSquareArea(10,12)
+circleArea:float = FigureArea.getCircleArea(2)
+triangleArea:float = FigureArea.getTriangleArea(5,2)
+squareArea:float = FigureArea.getSquareArea(10,12)
 
 figureAreas = {
     "площадь круга": circleArea,
---
+++ practice_1/second-task.py
@@ -0,0 +1,60 @@
+# Написать программу, которая на вход получает два числа и
+# операцию, которую к ним нужно применить. Должны быть реализованы
+# следующие операции: +, -, /, //, abs – модуль, pow или ** – возведение в
+# степень. 
+
+from enum import Enum
+
+class Calculator(Enum):
+    SUM = "+"
+    SUB = "-"
+    DIV = "/"
+    DIV_INTEGER = "//"
+    ABS = "abs"
+    POW = "pow"
+
+    @staticmethod
+    def sum(a:float,b:float)->float:
+        return a+b
+    
+    @staticmethod
+    def sub(a:float,b:float)->float:
+        return a-b
+
+    @staticmethod
+    def div(a:float,b:float)->float:
+        return a/b
+    
+    @staticmethod
+    def div_integer(a:float,b:float)->float:
+        return a//b
+
+    @staticmethod
+    def abs(a:float)->float:
+        return abs(a)
+
+    @staticmethod
+    def pow(a:float,b:float)->float:
+        return a**b
+
+
+a, b = float(input()), float(input())
+symbol = input()
+result:float = 0
+match symbol:
+    case Calculator.SUM.value:
+        result = Calculator.sum(a,b)
+    case Calculator.SUB.value:
+        result = Calculator.sub(a,b)
+    case Calculator.DIV.value:
+        result = Calculator.div(a,b)
+    case Calculator.DIV_INTEGER.value:
+        result = Calculator.div_integer(a,b)
+    case Calculator.ABS.value:
+        result = Calculator.abs(a)
+    case Calculator.POW.value:
+        result = Calculator.pow(a,b)
+    case _:
+        raise ValueError("Неизвестная команда")
+print(result)
+
